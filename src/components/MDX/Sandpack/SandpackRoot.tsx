9fd2fd9e955c5d92e6a3f18a632309a3ad8d24c0
Dave McCabe <davemccabe@fb.com>
2023-10-24 15:36:55 -0700

    Scratch towards sandbox RSC

--- src/components/MDX/Sandpack/SandpackRoot.tsx
+++ src/components/MDX/Sandpack/SandpackRoot.tsx
@@ -10,6 +10,27 @@ import {CustomPreset} from './CustomPreset';
 import {createFileMap} from './createFileMap';
 import {CustomTheme} from './Themes';
 
+import {
+  SandpackLayout,
+  SandpackCodeEditor,
+  SandpackPreview,
+} from "@codesandbox/sandpack-react/unstyled";
+
+import {memo, useRef, useState} from 'react';
+import {flushSync} from 'react-dom';
+import {
+  useSandpack,
+  useActiveCode,
+} from '@codesandbox/sandpack-react/unstyled';
+import cn from 'classnames';
+
+import {IconChevron} from 'components/Icon/IconChevron';
+import {NavigationBar} from './NavigationBar';
+import {Preview} from './Preview';
+
+import {useSandpackLint} from './useSandpackLint';
+
+
 type SandpackProps = {
   children: React.ReactNode;
   autorun?: boolean;
@@ -78,20 +99,91 @@ function SandpackRoot(props: SandpackProps) {
   return (
     <div className="sandpack sandpack--playground w-full my-8" dir="ltr">
       <SandpackProvider
-        template="react"
+        template="node"
         files={files}
         theme={CustomTheme}
         options={{
           autorun,
           initMode: 'user-visible',
           initModeObserverOptions: {rootMargin: '1400px 0px'},
-          bundlerURL: 'https://1e4ad8f7.sandpack-bundler-4bw.pages.dev',
-          logLevel: SandpackLogLevel.None,
+          // bundlerURL: "https://sandpack-bundler.pages.dev" ,
+          // bundlerURL: 'https://1e4ad8f7.sandpack-bundler-4bw.pages.dev',
+          bundlerTimeOut: 5000,
+          // logLevel: SandpackLogLevel.None,
         }}>
-        <CustomPreset providedFiles={Object.keys(files)} />
+        <Contents providedFiles={Object.keys(files)} />
+        {/* <CustomPreset providedFiles={Object.keys(files)} /> */}
       </SandpackProvider>
     </div>
   );
 }
+
+function Contents({providedFiles}) {
+  const {lintErrors, lintExtensions} = useSandpackLint();
+  const {sandpack} = useSandpack();
+  const {code} = useActiveCode();
+  const {activeFile} = sandpack;
+  const lineCountRef = useRef<{[key: string]: number}>({});
+  if (!lineCountRef.current[activeFile]) {
+    lineCountRef.current[activeFile] = code.split('\n').length;
+  }
+  const lineCount = lineCountRef.current[activeFile];
+  const isExpandable = lineCount > 16;
+
+  return (
+    <SandboxShell
+      providedFiles={providedFiles}
+      lintErrors={lintErrors}
+      lintExtensions={lintExtensions}
+      isExpandable={isExpandable}
+    />
+  );
+}
+
+function  SandboxShell({
+  providedFiles,
+  lintErrors,
+  lintExtensions,
+  isExpandable,
+}: {
+  providedFiles: Array<string>;
+  lintErrors: Array<any>;
+  lintExtensions: Array<any>;
+  isExpandable: boolean;
+}) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <SandpackLayout className={cn(
+      !(isExpandable || isExpanded) && 'rounded-b-lg overflow-hidden',
+      isExpanded && 'sp-layout-expanded'
+    )}>
+      <Editor lintExtensions={lintExtensions} />
+      <Preview
+            className="order-last xl:order-2"
+            isExpanded={isExpanded}
+            lintErrors={lintErrors}
+          />
+      <SandpackPreview /> 
+   </SandpackLayout> 
+  )
+}
+
+const Editor = memo(function Editor({
+  lintExtensions,
+}: {
+  lintExtensions: Array<any>;
+}) {
+  return (
+    <SandpackCodeEditor
+      showLineNumbers
+      showInlineErrors
+      showTabs={false}
+      showRunButton={false}
+      extensions={lintExtensions}
+    />
+  );
+});
+
 
 export default SandpackRoot;
--- src/content/learn/index.md
+++ src/content/learn/index.md
@@ -2,52 +2,6 @@
 title: Quick Start
 ---
 
-<Intro>
-
-Welcome to the React documentation! This page will give you an introduction to the 80% of React concepts that you will use on a daily basis.
-
-</Intro>
-
-<YouWillLearn>
-
-- How to create and nest components
-- How to add markup and styles
-- How to display data
-- How to render conditions and lists
-- How to respond to events and update the screen
-- How to share data between components
-
-</YouWillLearn>
-
-## Creating and nesting components {/*components*/}
-
-React apps are made out of *components*. A component is a piece of the UI (user interface) that has its own logic and appearance. A component can be as small as a button, or as large as an entire page.
-
-React components are JavaScript functions that return markup:
-
-```js
-function MyButton() {
-  return (
-    <button>I'm a button</button>
-  );
-}
-```
-
-Now that you've declared `MyButton`, you can nest it into another component:
-
-```js {5}
-export default function MyApp() {
-  return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
-  );
-}
-```
-
-Notice that `<MyButton />` starts with a capital letter. That's how you know it's a React component. React component names must always start with a capital letter, while HTML tags must be lowercase.
-
 Have a look at the result:
 
 <Sandpack>
